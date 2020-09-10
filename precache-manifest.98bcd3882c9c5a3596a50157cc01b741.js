63d032272d322092ed31b3f1672e1411e6e04685
zhangkai <zkdtckk@gmail.com>
2020-09-09 21:19:13 -0700

    Updates

---
+++ precache-manifest.98bcd3882c9c5a3596a50157cc01b741.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef7217715b6d61bda16df79b440a301",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "32e3e3e8772cf1c3161a",
+    "url": "/react-portfolio/static/css/main.c7aaf556.chunk.css"
+  },
+  {
+    "revision": "60abbb3f066449d5f418",
+    "url": "/react-portfolio/static/js/2.6e550c6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.6e550c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e3e3e8772cf1c3161a",
+    "url": "/react-portfolio/static/js/main.08f2a7b5.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
